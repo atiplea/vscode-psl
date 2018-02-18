8d4592bb66e84d74162a958267ec0116452e669e
Alexander Tiplea <alexander.tiplea@ing.nl>
2018-02-18 14:57:19 +0100

    Add get

--- src/hostCommands/get2.ts
+++ src/hostCommands/get2.ts
@@ -1,26 +1,27 @@
 import * as vscode from 'vscode';
 import { HostCommand, CommandResult, getConnection, executeWithProgress, EnvType } from './hostCommand';
 import * as path from 'path';
+import * as fs from 'fs-extra';
 import * as environment from '../common/environment';
 
-export class Send extends HostCommand {
+export class Get extends HostCommand {
 
-	static readonly COMMAND = 'psl.sendElement';
+	static readonly COMMAND = 'psl.getElement';
 
 	envType: EnvType;
 	icon: string;
 
 	constructor() {
 		super();
-		this.envType = EnvType.Mutli;
-		this.icon = HostCommand.icons.SEND;
+		this.envType = EnvType.Single;
+		this.icon = HostCommand.icons.GET;
 	}
 
 	async dirHandle(directory: string): Promise<string[]> | undefined {
 		let options = {
 			defaultUri: vscode.Uri.file(directory),
 			canSelectMany: true,
-			openLabel: 'Send'
+			openLabel: 'Get'
 		};
 		let uris = await vscode.window.showOpenDialog(options)
 		if (!uris) return;
@@ -29,12 +30,14 @@ export class Send extends HostCommand {
 
 	async execute(file: string, env: environment.EnvironmentConfig): Promise<CommandResult[]> {
 		let results: CommandResult[];
-		await executeWithProgress(`${path.basename(file)} SEND`, async () => {
-			this.logWait(`${path.basename(file)} SEND to ${env.name}`);
+		await executeWithProgress(`${path.basename(file)} GET`, async () => {
+			this.logWait(`${path.basename(file)} GET from ${env.name}`);
 			let connection = await getConnection(env);
-			await connection.send(file);
+			let output = await connection.get(file);
+			await fs.writeFile(file, output);
+			this.logSuccess(`${path.basename(file)} GET from ${env.name} succeeded`);
 			connection.close();
-			this.logSuccess(`${path.basename(file)} SEND to ${env.name} succeeded`);
+			await vscode.window.showTextDocument(vscode.Uri.file(file), { preview: false });
 		});
 		return results;
 	}
